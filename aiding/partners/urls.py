f8e16d153ed6037afa4869c24f1869786cabd292
Aisha <aisqazcev@alum.us.es>
2023-03-05 23:05:03 +0100

    feature-issue#5: cambios

--- aiding/partners/urls.py
+++ aiding/partners/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('donation/', DonationView.as_view()),
     path('donation/<int:id>',DonationView.as_view()),
-    path('partners/', PartnerManagement.as_view()),
-    path('partners/<int:id>', PartnerManagement.as_view()),
+    path('', PartnerManagement.as_view()),
+    path('<int:id>', PartnerManagement.as_view()),
 
 ]
