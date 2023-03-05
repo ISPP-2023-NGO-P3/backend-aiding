c2f9d3fa0e408eee6e15cbc470036e6c711d4b41
marolmmar1 <marolmmar1@alum.us.es>
2023-03-05 19:09:13 +0100

    feature-issue #12: Relocated communnication to partners module

--- aiding/base/models.py
+++ aiding/base/models.py
@@ -1,17 +1,3 @@
 from django.db import models
 
-from enum import Enum
-
 # Create your models here.
-class ComunicationType(Enum):
-    TELEFONICA = 'TELEFÓNICA'
-    TELEMATICA = 'TELEMÁTICA'
-    PERSONAL = 'PERSONAL'
-    EMAIL = 'EMAIL'
-
-class Comunication(models.Model):
-    date = models.DateField(null = False)
-    comunication_type = models.CharField(
-        choices=[(t, t.value) for t in ComunicationType],
-        max_length=20,
-        null=False)
