ae76b296e8573caf94c76aee5dcfec0a0366e478
cardelbec <Kiolpor.kolopo@gmail.com>
2023-03-05 18:04:17 +0100

    feat #1: Login API and User model

--- aiding/base/views.py
+++ aiding/base/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+from django.utils.decorators import method_decorator
+from django.contrib.auth import authenticate, login, logout
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from .models import *
+import json
 
-# Create your views here.
+class LoginView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def post(self, request):
+        jd = json.loads(request.body)
+        user = authenticate(username = jd['username'], password = jd['password'])
+        print(user)
+        if user is not None:
+            login(request, user)
+            data = {'message' : 'Login successful!'}
+        else:
+            data = {'message' : 'Login unsuccessful'}
+        
+        return JsonResponse(data)
+
+class LogoutView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def post(self, request):
+        jd = json.loads(request.body)
+        if request.user.is_authenticated:
+            logout(request)
+            data = {'message' : 'Logout successful!'}
+        else:
+            data = {'message' : 'Logout unsuccessful'}
+        return JsonResponse(data)
+        
+        
