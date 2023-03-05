29cd88af24b17c905d3c6170c4eeed3fd61dd53d
Rick3600 <nadalrick@gmail.com>
2023-03-05 13:24:09 +0100

    feature #13: Connect to deployed database

---
+++ aiding/deploy_settings.py
@@ -0,0 +1,21 @@
+# This file is used to override settings for deploy
+import environ
+
+env= environ.Env()
+environ.env.read_env()
+
+root_password=env('ROOT_PASSWORD')
+db_url = env('DATABASE_URL')
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aiding_db',
+        'USER': 'root',
+        'PASSWORD': + root_password ,
+        'HOST': + db_url ,
+        'PORT': '5432',
+    }
+}
