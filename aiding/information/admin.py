35274081f01c90d3b93db120e058d8e4fe3751d9
Manuel Sánchez Rodríguez <mansanrod4@alum.us.es>
2023-03-11 14:05:57 +0100

    refactor #10: solving bad smels

--- aiding/information/admin.py
+++ aiding/information/admin.py
@@ -5,37 +5,66 @@ from .models import Advertisement, Multimedia, Resource, Section
 
 
 class SectionAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    list_filter = ('name', )
-    search_fields = ('name', )
+    list_display = ("name",)
+    list_filter = ("name",)
+    search_fields = ("name",)
 
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ('title', 'url', 'section')
-    list_filter = ('title', )
-    search_fields = ('title', )
+    list_display = ("title", "url", "section")
+    list_filter = ("title",)
+    search_fields = ("title",)
 
 
 class MultimediaAdmin(admin.ModelAdmin):
-    list_display = ('advertisement', 'multimedia', 'description')
-    list_filter = ('advertisement', )
-    search_fields = ('advertisement', )
+    list_display = ("advertisement", "multimedia", "description")
+    list_filter = ("advertisement",)
+    search_fields = ("advertisement",)
 
 
 class ResourceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'description', 'number',
-                    'street', 'city', 'latitude', 'longitude')
-    list_filter = ('id', 'title', 'description', 'number',
-                   'street', 'city', 'latitude', 'longitude')
-    search_fields = ('id', 'title', 'description', 'number',
-                     'street', 'city', 'latitude', 'longitude')
+    list_display = (
+        "id",
+        "title",
+        "description",
+        "number",
+        "street",
+        "city",
+        "latitude",
+        "longitude",
+    )
+    list_filter = (
+        "id",
+        "title",
+        "description",
+        "number",
+        "street",
+        "city",
+        "latitude",
+        "longitude",
+    )
+    search_fields = (
+        "id",
+        "title",
+        "description",
+        "number",
+        "street",
+        "city",
+        "latitude",
+        "longitude",
+    )
 
     def save_model(self, request, obj, form, change):
         if change:
             old_obj = self.model.objects.get(pk=obj.pk)
-            if old_obj.number != obj.number or old_obj.street != obj.street or old_obj.city != obj.city or old_obj.latitude != obj.latitude or old_obj.longitude != obj.longitude:
-                coord = Resource.get_coordinates(
-                    self, obj.street, obj.number, obj.city)
+            if (
+                old_obj.number != obj.number
+                or old_obj.street != obj.street
+                or old_obj.city != obj.city
+                or old_obj.latitude != obj.latitude
+                or old_obj.longitude != obj.longitude
+            ):
+                coord = Resource.get_coordinates(self, obj.street, obj.number, obj.city)
                 if isinstance(coord, Response):
                     return coord
                 else:
--- aiding/information/models.py
+++ aiding/information/models.py
@@ -8,21 +8,34 @@ from .validators import validate_file_extension
 
 
 class Section(models.Model):
-    name = models.CharField(unique=True, blank=False,
-                            null=False, max_length=100)
+    name = models.CharField(unique=True, blank=False, null=False, max_length=100)
     active = models.BooleanField(blank=False, null=False, default=False)
+
 
 class Advertisement(models.Model):
     title = models.CharField(unique=True, blank=False, null=False, max_length=200)
-    description = models.TextField(blank= False, null=False, max_length=5000)
-    url = models.URLField(null= True, blank= True)
-    section = models.ForeignKey(Section, related_name='section', on_delete = models.CASCADE)
-    front_page = models.ImageField(null=False, upload_to='information/advertisements', default="information/media/default.png", validators=[validate_file_extension])
+    description = models.TextField(blank=False, null=False, max_length=5000)
+    url = models.URLField(null=True, blank=True)
+    section = models.ForeignKey(
+        Section, related_name="section", on_delete=models.CASCADE
+    )
+    front_page = models.ImageField(
+        null=False,
+        upload_to="information/advertisements",
+        default="information/media/default.png",
+        validators=[validate_file_extension],
+    )
+
 
 class Multimedia(models.Model):
-    advertisement = models.ForeignKey(Advertisement, related_name='advertisement', on_delete = models.CASCADE)
-    multimedia = models.ImageField(blank=True, null=True, upload_to='information/multimedia')
-    description = models.TextField(blank= True, null=True,max_length=255)
+    advertisement = models.ForeignKey(
+        Advertisement, related_name="advertisement", on_delete=models.CASCADE
+    )
+    multimedia = models.ImageField(
+        blank=True, null=True, upload_to="information/multimedia"
+    )
+    description = models.TextField(blank=True, null=True, max_length=255)
+
 
 class Resource(models.Model):
     title = models.CharField(blank=False, null=False, max_length=100)
@@ -33,10 +46,16 @@ class Resource(models.Model):
     city = models.CharField(blank=False, max_length=100)
     additional_comments = models.CharField(blank=True, max_length=255)
 
-    latitude = models.FloatField(blank=True, max_length=255, validators=[
-                                 MaxValueValidator(90), MinValueValidator(-90)])
-    longitude = models.FloatField(blank=True, max_length=255, validators=[
-                                  MaxValueValidator(180), MinValueValidator(-180)])
+    latitude = models.FloatField(
+        blank=True,
+        max_length=255,
+        validators=[MaxValueValidator(90), MinValueValidator(-90)],
+    )
+    longitude = models.FloatField(
+        blank=True,
+        max_length=255,
+        validators=[MaxValueValidator(180), MinValueValidator(-180)],
+    )
 
     def get_coordinates(self, street, number, city):
         address = street
