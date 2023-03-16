ab27b3c499a69fb0322100cd053eecae42a1fca2
Rick3600 <nadalrick@gmail.com>
2023-03-16 16:39:09 +0100

    feature #13: Changed database

--- aiding/base/models.py
+++ aiding/base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Section(models.Model):
+    name = models.CharField(unique=True, blank=False, null=False, max_length=100)
+    active = models.BooleanField(blank=False, null=False, default=False)
