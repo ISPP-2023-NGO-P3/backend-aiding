4a3aa8e46ed19f96d1cd2c93b69d443b5d3b6c9c
cardelbec <Kiolpor.kolopo@gmail.com>
2023-03-17 12:23:04 +0100

    fix #1: QA review changes

--- aiding/base/views.py
+++ aiding/base/views.py
@@ -1,8 +1,7 @@
+import json
 from django.utils.decorators import method_decorator
 from django.contrib.auth import authenticate, login, logout
-from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-import json
 
 from rest_framework import views
 from rest_framework.response import Response
@@ -11,16 +10,18 @@ from rest_framework.status import (
     HTTP_401_UNAUTHORIZED as ST_401
 )
 
-
 class LoginView(views.APIView):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
+    @method_decorator(csrf_exempt)
     def post(self, request):
+        if request.user.is_authenticated:
+            data = {'message' : 'You are already logged in!'}
+            return Response(data, status=ST_401)
         jd = json.loads(request.body)
         user = authenticate(username = jd['username'], password = jd['password'])
-        print(user)
         if user is not None:
             login(request, user)
             data = {'message' : 'Login successful!'}
@@ -29,15 +30,15 @@ class LoginView(views.APIView):
             data = {'message' : 'Login unsuccessful'}
             return Response(data, status=ST_401)
 
-class LogoutView(View):
+class LogoutView(views.APIView):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
-
+    
     def post(self, request):
         if request.user.is_authenticated:
             logout(request)
             data = {'message' : 'Logout successful!'}
         else:
-            data = {'message' : 'Logout unsuccessful'}
+            data = {'message' : 'You are not logged in!'}
         return Response(data, status=ST_200)
