be20b2ec77a357fc555cf4df28ecec54b6a33a8c
Aisha <aisqazcev@alum.us.es>
2023-03-21 18:39:08 +0100

    fix#18: atencion al cliente listo + merge con develop

--- aiding/aiding/urls.py
+++ aiding/aiding/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('base/', include('base.urls')),
     path('information/',include('information.urls')),
     path('partners/',include('partners.urls')),
+    path('volunteers/',include('volunteers.urls')),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
--- aiding/base/admin.py
+++ aiding/base/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import User, Notification
+
+from .models import Contact, User, Notification
 
 admin.site.register(User)
 admin.site.register(Notification)
-from django.contrib import admin
-from .models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'datetime', 'subject', 'message', 'isAnswered')
