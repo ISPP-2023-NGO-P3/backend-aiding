7571d85a4decd7464ba69180ca4455a1c09f6bf3
Manuel Sánchez Rodríguez <mansanrod4@alum.us.es>
2023-03-24 00:47:15 +0100

    feat #27: add events location

--- aiding/events/models.py
+++ aiding/events/models.py
@@ -1,19 +1,75 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 from .validators import validate_event_date
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST as ST_400
+from rest_framework.status import HTTP_404_NOT_FOUND as ST_404
+from rest_framework.status import HTTP_408_REQUEST_TIMEOUT as ST_408
 
 
 class Event(models.Model):
+
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
-    address = models.CharField(max_length=100)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
-    places = models.IntegerField()
+    places = models.PositiveIntegerField()
+
+    street = models.CharField(blank=False, max_length=255)
+    number = models.CharField(null=True, blank=True, max_length=10)
+    city = models.CharField(blank=False, max_length=100)
+
+    latitude = models.FloatField(
+        null=True,
+        blank=True,
+        max_length=255,
+        validators=[MaxValueValidator(90), MinValueValidator(-90)],
+    )
+    longitude = models.FloatField(
+        null=True,
+        blank=True,
+        max_length=255,
+        validators=[MaxValueValidator(180), MinValueValidator(-180)],
+    )
+
+    position = models.CharField(max_length=50, blank=True)
 
     def clean(self):
         validate_event_date(self.start_date, self.end_date)
 
-    ### Aclarar que address se sustituirá o no por una asociacion a Recursos
-    ### Hará falta una asociación a los usuarios que quieran asistir y así se
-    ### podrá controlar el número de plazas disponibles
+    def save(self, *args, **kwargs):
+        self.position = f'[{self.latitude}, {self.longitude}]'
+        super(Event, self).save(*args, **kwargs)
+
+    def get_coordinates(self, street, number, city):
+        address = street
+        if number:
+            if number.isdigit():
+                if int(number) > 0:
+                    address += ", " + number
+                else:
+                    error = "Number must be positive or null"
+                    return Response(data=error, status=ST_400)
+            else:
+                error = "Number must be positive or null"
+                return Response(data=error, status=ST_400)
+
+        address += ", " + city
+        geolocator = Nominatim(user_agent="aiding")
+        location = geolocator.geocode(address)
+        try:
+            location = geolocator.geocode(address, timeout=10)
+        except GeocoderTimedOut as error_timeout:
+            error = "GeocodeTimedOut: " + str(error_timeout)
+            return Response(data=error, status=ST_408)
+
+        if location:
+            latitude = location.latitude
+            longitude = location.longitude
+        else:
+            error = "Address not found."
+            return Response(data=error, status=ST_404)
+        return latitude, longitude
