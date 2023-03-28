b8baf4bf9477124e4d7c78ec414074b2ce32355a
rosmolarr <rosamolina100@gmail.com>
2023-03-28 20:17:08 +0200

    Fixed some errors in settings

--- aiding/aiding/settings.py
+++ aiding/aiding/settings.py
@@ -160,7 +160,7 @@ INSTALLED_APPS = INSTALLED_APPS + MODULES
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'api/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -168,7 +168,7 @@ STATIC_URL = 'api/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
-MEDIA_URL="/api/media/"
+MEDIA_URL="/media/"
 
 
 
