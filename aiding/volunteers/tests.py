c79cd4f0fec972431e4fa57d6b4bffcb5b1ac43e
rosmolarr <rosamolina100@gmail.com>
2023-04-09 16:26:04 +0200

    feat #35: Hechos test de turno

--- aiding/volunteers/tests.py
+++ aiding/volunteers/tests.py
@@ -5,8 +5,9 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.test import APIClient
 from base.models import User
 import json
+import datetime
 
-""" class VolunteerTests(APITestCase):
+class VolunteerTests(APITestCase):
     
     def setUp(self):
         self.client = APIClient()
@@ -199,15 +200,15 @@ import json
         response = self.client.delete('/volunteers/1')
         self.assertEqual(response.status_code, 404)
         dataMessage = {'error': 'Volunteer not found...'}
-        self.assertEqual(response.data, dataMessage) """
+        self.assertEqual(response.data, dataMessage) 
 
 class TurnTests(APITestCase):
     
     def setUp(self):
         self.client = APIClient()
         self.user = User.objects.create_user(
-            username="ispp", 
-            password="ispp"
+            username="rosa", 
+            password="rosa"
         )
         self.client.force_authenticate(user=self.user)
 
@@ -216,26 +217,24 @@ class TurnTests(APITestCase):
     def test_list_positive_turns_status_OK(self):
         Turn.objects.create(date="2023-05-02", startTime="08:00", endTime="14:00")
         Turn.objects.create(date="2023-05-02", startTime="14:00", endTime="20:00")
-        response = self.client.get('/volunteers/turns')
-        print(response.status_code)
-        print(response)
+        response = self.client.get('/volunteers/turns/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(json.loads(response.content)), 2)
         
-    """ def test_list_negative_turns_status_NOT_FOUND(self):
-        response = self.client.get('/volunteers/turns')
+    def test_list_negative_turns_status_NOT_FOUND(self):
+        response = self.client.get('/volunteers/turns/')
         data = {"message": "turn not found..."}
         self.assertEqual(response.status_code, 404)
         self.assertEqual(json.loads(response.content), data)
         
     def test_show_positive_turns_status_OK(self):
         turn=Turn.objects.create(date="2023-05-02", startTime="08:00", endTime="14:00")
-        response = self.client.get(f'/volunteers/turns/{turn.id}')
+        response = self.client.get(f'/volunteers/turns/{turn.id}/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["name"], "Turno1")
+        self.assertEqual(response.data["date"], datetime.date(2023, 5, 2))
         
     def test_show_negative_turns_status_NOT_FOUND(self):
-        response = self.client.get('/volunteers/turns/1')
+        response = self.client.get('/volunteers/turns/1/')
         data = {"message": "turn not found..."}
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.data, data)
@@ -245,14 +244,14 @@ class TurnTests(APITestCase):
     def test_create_positive_turns_CREATED(self):
         data = JSONRenderer().render({"date":"2023-05-02", 
                                       "startTime":"08:00", "endTime":"14:00"}).decode("utf-8")
-        response = self.client.post('/volunteers/turns', data=data, content_type='application/json')
+        response = self.client.post('/volunteers/turns/', data=data, content_type='application/json')
         self.assertEqual(response.status_code, 201)
         dataMessage = {'message': 'Success'}
         self.assertEqual(response.data, dataMessage)
     
     def test_create_negative_turns_wrong_date(self):
         data = JSONRenderer().render({"date":"2023-05-02", "startTime":"08:00", "endTime":"7:00"}).decode("utf-8")
-        response = self.client.post('/volunteers/turns', data=data, content_type='application/json')
+        response = self.client.post('/volunteers/turns/', data=data, content_type='application/json')
         self.assertEqual(response.status_code, 409)
         dataMessage = {'error': 'La hora de inicio debe ser anterior a la hora de fin'}
         self.assertEqual(response.data, dataMessage)
@@ -262,25 +261,23 @@ class TurnTests(APITestCase):
     def test_update_positive_turns_status_OK(self):
         turn = Turn.objects.create(date="2023-05-02", startTime="08:00", endTime="14:00")
         data = JSONRenderer().render({"date":"2023-05-03", "startTime":"08:00", "endTime":"14:00"}).decode("utf-8")
-        response = self.client.put(f'/volunteers/turns/{turn.id}', data=data, content_type='application/json')
+        response = self.client.put(f'/volunteers/turns/{turn.id}/', data=data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
         dataMessage = {'message': 'Success'}
         self.assertEqual(response.data, dataMessage)
     
     def test_update_negative_turns_status_NOT_FOUND(self):
         data = JSONRenderer().render({"date":"2023-05-02", "startTime":"08:00", "endTime":"14:00"}).decode("utf-8")
-        response = self.client.put('/volunteers/turns/1', data=data, content_type='application/json')
-        print("AQUIIIIIIIIIIIII-------------------------------------------------------------------")
-        print(response)
-        self.assertEqual(response.status_code, 404)
-        dataMessage = {'error': 'Turn not found...'}
+        response = self.client.put('/volunteers/turns/1/', data=data, content_type='application/json')
+        self.assertEqual(response.status_code, 409)
+        dataMessage = {'message': 'Turn not found...'}
         
         self.assertEqual(response.data, dataMessage)
         
     def test_update_negative_turns_wrong_date(self):
         turn = Turn.objects.create(date="2023-05-02", startTime="08:00", endTime="14:00")
         data = JSONRenderer().render({"date":"2023-05-02", "startTime":"08:00", "endTime":"7:00"}).decode("utf-8")
-        response = self.client.put(f'/volunteers/turns/{turn.id}', data=data, content_type='application/json')
+        response = self.client.put(f'/volunteers/turns/{turn.id}/', data=data, content_type='application/json')
         self.assertEqual(response.status_code, 409)
         dataMessage = {'error': 'La hora de inicio debe ser anterior a la hora de fin'}
         self.assertEqual(response.data, dataMessage)
@@ -289,14 +286,13 @@ class TurnTests(APITestCase):
     
     def test_delete_positive_turns_status_OK(self):
         turn = Turn.objects.create(date="2023-05-02", startTime="08:00", endTime="14:00")
-        response = self.client.delete(f'/volunteers/turns/{turn.id}')
+        response = self.client.delete(f'/volunteers/turns/{turn.id}/')
         self.assertEqual(response.status_code, 204)
-        dataMessage = {'message': 'Successfully deleted'}
+        dataMessage = {'message': 'Success'}
         self.assertEqual(response.data, dataMessage)
         
     def test_delete_negative_turns_status_NOT_FOUND(self):
-        response = self.client.delete('/volunteers/turns/1')
-        self.assertEqual(response.status_code, 404)
-        dataMessage = {'error': 'Turn not found...'}
-        self.assertEqual(response.data, dataMessage) """
-            
+        response = self.client.delete('/volunteers/turns/1/')
+        self.assertEqual(response.status_code, 409)
+        dataMessage = {'message': 'Turn not found...'}
+        self.assertEqual(response.data, dataMessage) 
