b40f0e6ae71a03db6e3b924d3663e70c233b6b71
Josdelsan <joseM_DS@hotmail.com>
2023-04-26 00:19:13 +0200

    Bosco deploy commit

--- aiding/aiding/urls.py
+++ aiding/aiding/urls.py
@@ -26,11 +26,11 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/', include('base.urls')),
-    path('information/',include('information.urls')),
+    #path('information/',include('information.urls')),
     path('partners/',include('partners.urls')),
     path('volunteers/',include('volunteers.urls')),
-    path('events/',include('events.urls')),
-    path('stock/',include('stock.urls')),
+    #path('events/',include('events.urls')),
+    #path('stock/',include('stock.urls')),
     path("token/", TokenObtainPairView.as_view(), name="obtain_token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
